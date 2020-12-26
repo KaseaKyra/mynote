3a4f11698bb33a11b1e32a197a5d8114b53bc137
yen.dinhhai <dinhyensamagada98@gmail.com>
2020-12-26 15:48:36 +0700

    2 delete note_view_item_edit.dart and edit note_view_item.dart for create and update action.

--- lib/ui/views/note/widgets/note_view_item.dart
+++ lib/ui/views/note/widgets/note_view_item.dart
@@ -1,29 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:mynote/ui/views/note/note_model.dart';
 import 'package:mynote/ui/views/note/note_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
 class NoteViewItem extends ViewModelWidget<NoteViewModel> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context, model) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chi tiết ${model.editingItem.id}'),
+        title: Text('Chi tiết'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () => model.state = NoteViewState.listView,
         ),
         actions: [
           IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () => model.updateItem())
+            icon: Icon(Icons.save),
+            onPressed: () => model.saveItem(),
+          )
         ],
       ),
-      body: Center(
-        child: ListTile(
-          title: Text(model.editingItem.title),
-          subtitle: Text(model.editingItem.desc),
-        ),
+      body: Column(
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    autocorrect: true,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      hintText: 'Nhập tiêu đề',
+                    ),
+                    validator: (value) {
+                      return value.isEmpty ? 'Không được để trống!' : null;
+                    },
+                    controller: model.editingControllerTitle,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Nhập mô tả',
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      return value.isEmpty ? 'Không được để trống!' : null;
+                    },
+                    controller: model.editingControllerDesc,
+                    maxLines: 10,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
